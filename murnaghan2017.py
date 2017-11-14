10dad176caf7dc42563f5724e7b8de67c600e718
caseynbrock <casey.brock@vanderbilt.edu>
2017-11-14 17:55:42 -0600

    Move move previously written functions to methods of LatticeVectorSweep. Update tests.

--- murnaghan2017.py
+++ murnaghan2017.py
@@ -75,7 +75,111 @@ class LatticeParameterSweep():
         chat = [c1, c2, c3]
         return np.array([ahat, bhat, chat])
 
-def generate_lattice_constants(lattice_parameter_sweep):
+    def preprocess_file(self, s):
+        """
+        wraps the specific file preprocess functions for different dft codes
+        """
+        if self.energy_driver=='abinit':
+            self._preprocess_file_abinit(s)
+        else:
+            raise ValueError('Unknown energy driver specified')
+
+    def _preprocess_file_abinit(self, s):
+        """
+        writes abinit.in from template and appends lattice vector lengths (acell) and 
+        EITHER angles (angdeg) or primitive vectors (rprim), depending on which was 
+        passed in to the LatticeVectorSweep object
+        """
+        shutil.copy2(self.template_file, 'abinit.in')
+        if self.angles is not None:
+            with open('abinit.in', 'a') as f:
+                f.write('acell ' + ' '.join([str(float(n)) for n in s]) + '\n')
+                f.write('angdeg ' + ' '.join([str(float(n)) for n in self.angles]) + '\n')
+        else:
+            with open('abinit.in', 'a') as f:
+                f.write('acell ' + ' '.join([str(float(n)) for n in s]) + '\n')
+                f.write('rprim ' + ' '.join([str(float(n)) for n in self.prim_vec[0]]) + '\n')
+                f.write('      ' + ' '.join([str(float(n)) for n in self.prim_vec[1]]) + '\n')
+                f.write('      ' + ' '.join([str(float(n)) for n in self.prim_vec[2]]) + '\n')
+
+    def run_energy_calculations(abc, angles, template_file, energy_driver):
+        """
+        Uses DFT code to calculate energy at each of the lattice constants specified.
+    
+        For each lattice constant in sweep, sets up directory and runs dft code in that directory.
+        """  
+        # remove old work directories
+        for f in glob.glob('workdir.*'):
+            shutil.rmtree(f)
+        
+        # run dft calculations and return calculated energies
+        main_dir = os.getcwd()
+        energy_list_hartree = []
+        for i,s in enumerate(abc):
+            dir_name = 'workdir.'+str(i)
+            os.mkdir(dir_name)
+            os.chdir(dir_name)
+            preprocess_file(s, angles, template_file, energy_driver)
+            # run_dft
+            os.chdir(main_dir)
+            #energy_list_hartree.append(abinit_get_energy())
+        return energy_list_hartree
+    
+    def fit_to_murnaghan(vol_array, E_array):
+            ### first, fit a parabola to the data
+            # y = ax^2 + bx + c
+            a, b, c = np.polyfit(vol_array, E_array, 2)
+            #
+            # the parabola does not fit the data very well, but we can use it to get
+            # some analytical guesses for other parameters.
+            # V0 = minimum energy volume, or where dE/dV=0
+            # E = aV^2 + bV + c
+            # dE/dV = 2aV + b = 0
+            # V0 = -b/2a
+            # E0 is the minimum energy, which is:
+            # E0 = aV0^2 + bV0 + c
+            # B is equal to V0*d^2E/dV^2, which is just 2a*V0
+            # and from experience we know Bprime_0 is usually a small number like 4
+            V0_guess = -b/(2*a)
+            E0_guess = a*V0_guess**2. + b*V0_guess + c
+            B0_guess = 2.*a*V0_guess
+            BP_guess = 4.
+    
+            murnpars_guess = [E0_guess, B0_guess, BP_guess, V0_guess]
+            murnpars = leastsq(objective, murnpars_guess, args=(E_array,vol_array))
+            return murnpars
+
+    def run_abinit():
+        #call abinit
+        with open('log', 'w') as log_fout, open('files','r') as files_fin:
+            #subprocess.call(['srun', '-n', '64', 'abinit'], stdin=files_fin, stdout=log_fout)
+            subprocess.call(['abinit'], stdin=files_fin, stdout=log_fout)
+     
+        # read energy from log
+        energy = abinit_get_energy()
+    
+    def abinit_get_energy():
+        with open('log', 'r') as log_fin:
+            for line in log_fin.readlines():
+                if ' etotal ' in line:
+                    abinit_energy_hartree = np.float(line.split()[1])
+        return abinit_energy_hartree
+    
+    def read_energy_results():
+        """
+        gets total energy from abinit output in all wrokdirs
+        """
+        pass
+        
+    def write_results(raw_energy_data):
+        """ 
+        writes raw energy data from dft runs.
+        The raw data input should have a,b,c,E for each run
+        """
+        pass
+
+
+def generate_lattice_constants(abc_guess, max_pert, N, two_dim=False):
     """
     generates an Nx3 numpy array containing the three lattice constants for each energy calculation. 
     Each value will range from (1-max_pert)*abc_guess[i] to (1+max_pert)*abc_guess[i].
@@ -94,99 +198,3 @@ def generate_lattice_constants(lattice_parameter_sweep):
     return abc
 
 
-def run_energy_calculations(abc, angles, template_file, energy_driver):
-    """
-    Uses DFT code to calculate energy at each of the lattice constants specified.
-
-    For each lattice constant in sweep, sets up directory and runs dft code in that directory.
-    """  
-    # remove old work directories
-    for f in glob.glob('workdir.*'):
-        shutil.rmtree(f)
-    
-    # run dft calculations and return calculated energies
-    main_dir = os.getcwd()
-    energy_list_hartree = []
-    for i,s in enumerate(abc):
-        dir_name = 'workdir.'+str(i)
-        os.mkdir(dir_name)
-        os.chdir(dir_name)
-        preprocess_file(s, angles, template_file, energy_driver)
-        # run_dft
-        os.chdir(main_dir)
-        #energy_list_hartree.append(abinit_get_energy())
-    return energy_list_hartree
-
-def fit_to_murnaghan(vol_array, E_array):
-        ### first, fit a parabola to the data
-        # y = ax^2 + bx + c
-        a, b, c = np.polyfit(vol_array, E_array, 2)
-        #
-        # the parabola does not fit the data very well, but we can use it to get
-        # some analytical guesses for other parameters.
-        # V0 = minimum energy volume, or where dE/dV=0
-        # E = aV^2 + bV + c
-        # dE/dV = 2aV + b = 0
-        # V0 = -b/2a
-        # E0 is the minimum energy, which is:
-        # E0 = aV0^2 + bV0 + c
-        # B is equal to V0*d^2E/dV^2, which is just 2a*V0
-        # and from experience we know Bprime_0 is usually a small number like 4
-        V0_guess = -b/(2*a)
-        E0_guess = a*V0_guess**2. + b*V0_guess + c
-        B0_guess = 2.*a*V0_guess
-        BP_guess = 4.
-
-        murnpars_guess = [E0_guess, B0_guess, BP_guess, V0_guess]
-        murnpars = leastsq(objective, murnpars_guess, args=(E_array,vol_array))
-        return murnpars
-
-def run_abinit():
-    #call abinit
-    with open('log', 'w') as log_fout, open('files','r') as files_fin:
-        #subprocess.call(['srun', '-n', '64', 'abinit'], stdin=files_fin, stdout=log_fout)
-        subprocess.call(['abinit'], stdin=files_fin, stdout=log_fout)
- 
-    # read energy from log
-    energy = abinit_get_energy()
-
-def abinit_get_energy():
-    with open('log', 'r') as log_fin:
-        for line in log_fin.readlines():
-            if ' etotal ' in line:
-                abinit_energy_hartree = np.float(line.split()[1])
-    return abinit_energy_hartree
-
-def read_energy_results():
-    """
-    gets total energy from abinit output in all wrokdirs
-    """
-    pass
-
-
-def preprocess_file(s, angles, template_file, energy_driver):
-    """
-    wraps the specific file preprocess functions for different dft codes
-    """
-    if energy_driver=='abinit':
-        _preprocess_file_abinit(s, angles, template_file)
-    else:
-        raise ValueError('Unknown energy driver specified')
-
-
-def _preprocess_file_abinit(s, angles, template_file):
-    """
-    writes abinit.in from template and appends lattice vector lengths (acell) and angles (angdeg)
-    """
-    shutil.copy2(template_file, 'abinit.in')
-    with open('abinit.in', 'a') as f:
-        f.write('acell ' + ' '.join([str(float(n)) for n in s]) + '\n')
-        f.write('angdeg ' + ' '.join([str(float(n)) for n in angles]) + '\n')
-
-    
-def write_results(raw_energy_data):
-    """ 
-    writes raw energy data from dft runs.
-    The raw data input should have a,b,c,E for each run
-    """
-    pass
--- tests/test_murnaghan2017.py
+++ tests/test_murnaghan2017.py
@@ -8,30 +8,93 @@ import murnaghan2017 as m
 test_dir = os.path.dirname(os.path.realpath(__file__))
 input_dir = os.path.join(test_dir, 'input_files')
 
+def test_angles_prim_vec_neither():
+    """
+    raises value error if neither of angles and prim_vec are specified
+    """
+    energy_driver = None
+    template_file = None
+    abc = None
+    with pytest.raises(ValueError):
+        sweep = m.LatticeParameterSweep(energy_driver, template_file, abc)
+
+def test_angles_prim_vec_both():
+    """
+    raises value error if both angles and prim_vec are specified
+    """
+    energy_driver = None
+    template_file = None
+    abc = None
+    with pytest.raises(ValueError):
+        sweep = m.LatticeParameterSweep(energy_driver, template_file, abc, angles=1, prim_vec=1)
+
+def test_angles_None_when_prim_vec_specified():
+    """
+    if prim_vec is specified, self.angles=None
+    """
+    energy_driver = None
+    template_file = None
+    abc = None
+    sweep = m.LatticeParameterSweep(energy_driver, template_file, abc, prim_vec=1)
+    assert sweep.angles is None
+
 def test_preprocess_file_bad():
     """
     entering bad name for energy driver raises value error
     """
+    energy_driver = 'dummy'
+    s_dummy = [1,2,3]
+    sweep = m.LatticeParameterSweep(energy_driver, '', [], prim_vec=[])
     with pytest.raises(ValueError):
-        m.preprocess_file(0, 0, 'dummyfilename', 'asdfas')
+        sweep.preprocess_file(s_dummy)
 
-def test_preprocess_file_abinit():
+def test_preprocess_file_abinit_angdeg():
     """
     calling _preprocess_file with for abinit correctly writes lattice constants and angles to abinit input file.
     
     not very robust since different number format would cause a fail
     """
+    energy_driver = 'abinit'
     template_file = os.path.join(input_dir, 'abinit.in.template.example')
     correct_file = os.path.join(input_dir, 'abinit.in.correct')
-    # preprocess
     s = [0.1, 0.2, 0.3]
     ang = [90, 90, 120]
-    m.preprocess_file(s, ang, template_file, 'abinit')
+    sweep = m.LatticeParameterSweep(energy_driver, template_file, s, angles=ang)
+    sweep.preprocess_file(s)
     # compare written abinit file with correct input file
     with open(correct_file) as f1, open('abinit.in') as f2:
         assert f1.readlines() == f2.readlines()
     os.remove('abinit.in') 
 
+def test_preprocess_file_abinit_rprim():
+    """
+    calling _preprocess_file with for abinit correctly writes lattice constants and angles to abinit input file.
+    
+    not very robust since different number format would cause a fail
+    """
+    energy_driver = 'abinit'
+    template_file = os.path.join(input_dir, 'abinit.in.template.example')
+    correct_file = os.path.join(input_dir, 'abinit.in.correct.2')
+    s = [0.1, 0.2, 0.3]
+    prim_vec = [[1,1,0], [0,2,2], [3,0,3]]
+    sweep = m.LatticeParameterSweep(energy_driver, template_file, s, prim_vec=prim_vec)
+    sweep.preprocess_file(s)
+    # compare written abinit file with correct input file
+    with open(correct_file) as f1, open('abinit.in') as f2:
+        assert f1.readlines() == f2.readlines()
+    os.remove('abinit.in') 
+
+def test_generate_lattice_constants():
+    """ 
+    generates numpy array of lattice paramters correctly
+    """
+    abc_guess = [0.5, 1, 2.0]
+    pert = 0.05
+    N = 5
+    abc = m.generate_lattice_constants(abc_guess, pert, N)
+    abc_correct = np.array([[0.475, 0.95, 1.9], [0.4875, 0.975, 1.95],
+        [0.5, 1., 2.], [0.5125, 1.025, 2.05], [0.525, 1.05, 2.1]])
+    assert (abc == abc_correct).all()
 def test_generate_lattice_constants():
     """ 
     generates numpy array of lattice paramters correctly
