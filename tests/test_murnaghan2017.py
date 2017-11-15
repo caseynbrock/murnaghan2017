81313b1a8f2505f0632678b70bca3fa01af99d0f
caseynbrock <casey.brock@vanderbilt.edu>
2017-11-15 13:12:47 -0600

    Add TemporaryDirectory class for dealing with file system writing tests

--- tests/test_murnaghan2017.py
+++ tests/test_murnaghan2017.py
@@ -4,10 +4,28 @@ import pytest
 import numpy as np
 import os
 import shutil
+from contextlib import contextmanager
+import tempfile
 import murnaghan2017 as m
 
+main_dir = os.getcwd()
 test_dir = os.path.dirname(os.path.realpath(__file__))
 input_dir = os.path.join(test_dir, 'input_files')
+
+class TemporaryDirectory(object):
+    """
+    Context manager for tempfile.mkdtemp() so it's usable with 'with' statement.
+    This version automatically changes to and leaves temporary directory.
+    """
+    def __enter__(self):
+        self.start_dir = os.getcwd()
+        self.name = tempfile.mkdtemp()
+        os.chdir(self.name)
+        return self.name
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        os.chdir(self.start_dir)
+        shutil.rmtree(self.name)
 
 def test_angles_prim_vec_neither():
     """
@@ -66,6 +84,24 @@ def test_preprocess_file_abinit_angdeg():
     with open(correct_file) as f1, open('abinit.in') as f2:
         assert f1.readlines() == f2.readlines()
     os.remove('abinit.in') 
+
+def test_preprocess_file_abinit_angdeg2():
+    """
+    calling _preprocess_file with for abinit correctly writes lattice constants and angles to abinit input file.
+    
+    not very robust since different number format would cause a fail
+    """
+    with TemporaryDirectory() as tmp_dir:
+        energy_driver = 'abinit'
+        template_file = os.path.join(input_dir, 'abinit.in.template.example')
+        correct_file = os.path.join(input_dir, 'abinit.in.correct')
+        s = [0.1, 0.2, 0.3]
+        ang = [90, 90, 120]
+        sweep = m.LatticeParameterSweep(energy_driver, template_file, s, angles=ang)
+        sweep.preprocess_file(s)
+        # compare written abinit file with correct input file
+        with open(correct_file) as f1, open('abinit.in') as f2:
+            assert f1.readlines() == f2.readlines()
 
 def test_preprocess_file_abinit_rprim():
     """
@@ -165,7 +201,7 @@ def test_abinit_get_energy_single():
     
 
 def test_abinit_get_energy_multiple():
-    """When only single etotal reported (typical case), returns etotal in Hartree"""
+    """When multiple etotal reported (as in structure relaxation), returns FINAL etotal in Hartree"""
     energy_driver='abinit'
     template_file = None
     abc = None
@@ -178,28 +214,28 @@ def test_abinit_get_energy_multiple():
     os.chdir('..')
     assert np.isclose(E, -7.3655263854)
     
-
-
-#     shutil.copy(os.path.join(input_dir, 'log.structurerelax.example'), '.')
-
-
-
-def test_run_abinit():
-    assert False
-
 def test_fit_to_murnaghan():
     assert False
 
-def test_run_energy_calculations_abinit():
+
+def test_run_energy_calculations_abinit2():
     """
     sets up files and runs abinit for each lattice constant
     
     Requires abinit set up correctly
     """
-    energy_driver = 'abinit'
-    template_file = os.path.join(input_dir, 'abinit.in.template.Li')
-    abc = np.array([[a*pert for a in 3*[3.3]] for pert in [0.95, 1, 1.05]])
-    prim_vec = [[0.5,0.5,-0.5], [-0.5,0.5,0.5], [0.5,-0.5,0.5]]
-    sweep = m.LatticeParameterSweep(energy_driver, template_file, abc, prim_vec=prim_vec)
-    sweep.run_energy_calculations()
-    raise Exception('Need to finish test after other methods written')
+    with TemporaryDirectory() as tmp_dir:
+        # set up example input files in temporary directory
+        os.chdir(tmp_dir)
+        os.mkdir('templatedir')
+        shutil.copy(os.path.join(input_dir, 'files.example.Li'), 
+                    os.path.join('templatedir', 'files'))
+
+        # run sweep  in tmp_dir
+        energy_driver = 'abinit'
+        template_file = os.path.join(input_dir, 'abinit.in.template.Li')
+        abc = np.array([[a*pert for a in 3*[3.3]] for pert in [0.95, 1, 1.05]])
+        prim_vec = [[0.5,0.5,-0.5], [-0.5,0.5,0.5], [0.5,-0.5,0.5]]
+        sweep = m.LatticeParameterSweep(energy_driver, template_file, abc, prim_vec=prim_vec)
+        sweep.run_energy_calculations()
+    pass # just gonna assume if it gets to here without error, then nothing is wrong
